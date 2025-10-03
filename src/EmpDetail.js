13b68e8d56f4ba179f53d401691db7de8201ffe7
oktochoi <okto0914@gmail.com>
2025-10-03 18:50:46 +0900

    fd

---
+++ src/EmpDetail.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const EmpDetail = () => {
+  const { empid } = useParams();
+  const [empdata, setEmpdata] = useState({});
+
+  useEffect(() => {
+    fetch("https://68ddd07fd7b591b4b78d8a2d.mockapi.io/employee/" + empid)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        setEmpdata(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div>
+      <div className="card" style={{ textAlign: "left" }}>
+        <div className="card-title">
+          <h2>Employee Create</h2>
+        </div>
+        <div className="card-body"></div>
+        {empdata && (
+          <div>
+            <h2>
+              The Employee name is : <b>{empdata.name}</b> ({empdata.id})
+            </h2>
+            <h3>Contact Details</h3>
+            <h5>
+              Email is : <b>{empdata.email}</b>
+            </h5>
+            <h5>
+              Phone is : <b>{empdata.phone}</b>
+            </h5>
+            <h5>
+              Age is : <b>{empdata.age}</b>
+            </h5>
+            <Link className="btn btn-danger" to="/">
+              Back to List
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EmpDetail;
---
+++ src/EmpListing.js
@@ -0,0 +1,110 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const EmpListing = () => {
+  const [empdata, setEmpdata] = useState(null);
+  const navigate = useNavigate();
+
+  const LoadDetail = (id) => {
+    navigate("/employee/detail/" + id);
+  };
+  const LoadEdit = (id) => {
+    navigate("/employee/edit/" + id);
+  };
+  const Removefunction = (id) => {
+    if (window.confirm("Do you want to remove?")) {
+      fetch("https://68ddd07fd7b591b4b78d8a2d.mockapi.io/employee/" + id, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          alert("Removed successfully.");
+          window.location.reload();
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://68ddd07fd7b591b4b78d8a2d.mockapi.io/employee")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        setEmpdata(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>직원 명단</h2>
+        </div>
+        <div className="card-body">
+          <div className="divbtn">
+            <Link to="employee/create" className="btn btn-success">
+              Add New (+)
+            </Link>
+          </div>
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Age</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {empdata &&
+                empdata.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.age}</td>
+                    <td>
+                      <a
+                        onClick={() => {
+                          LoadEdit(item.id);
+                        }}
+                        className="btn btn-success"
+                      >
+                        수정
+                      </a>
+                      <a
+                        onClick={() => {
+                          Removefunction(item.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        삭제
+                      </a>
+                      <a
+                        onClick={() => {
+                          LoadDetail(item.id);
+                        }}
+                        className="btn btn-primary"
+                      >
+                        세부 사항
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmpListing;
